65605b1133a2a6adf1a010bc68654c9291f6d5e2
Adrian G <77066095+gruendlich@users.noreply.github.com>
2026-01-22 16:03:28 +0100

    feat: Adds condition 1
    Concerns #8 of #2

--- src/main/java/com/skarbalius/Conditions.java
+++ src/main/java/com/skarbalius/Conditions.java
@@ -28,5 +28,34 @@ public class Conditions
         return false;
     }
 
+    public boolean condition1(ArrayList<Point> points, int NUMPOINTS, Parameters_T parameters) {
+        Point center;
+
+        for (int i = 2; i < NUMPOINTS; i++) {
+            ArrayList<Point> consecutive_points = new ArrayList<Point>(3);
+            for (int j = i - 2; j <= i; j++) {
+                consecutive_points.add(points.get(j));
+            }
+
+            center = consecutive_points.stream()
+                    .reduce(new Point(0, 0), (acc, p) -> {
+                        acc.x += p.x;
+                        acc.y += p.y;
+                        return acc;
+                    });
+            center.x /= 3;
+            center.y /= 3;
+
+            Point finalCenter = center;
+            boolean min_one_outside = consecutive_points.stream()
+                    .anyMatch(point -> point.getDistance(finalCenter) > parameters.RADIUS1);
+
+            if (min_one_outside) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
