c41210ccb17882460a53315fb6ac3c1a3b1e68e7
Juozas Skarbalius <juozas2003@gmail.com>
2026-01-26 14:34:04 +0100

    #1 add primitive task handler test

--- src/main/java/com/skarbalius/task/TaskHandler.java
+++ src/main/java/com/skarbalius/task/TaskHandler.java
@@ -2,6 +2,7 @@ package com.skarbalius.task;
 
 public abstract class TaskHandler {
     public TaskHandler nextHandler;
+    private TaskContext lastContext;
 
     void setNextHandler(TaskHandler nextHandler) {
         this.nextHandler = nextHandler;
@@ -10,8 +11,13 @@ public abstract class TaskHandler {
     public abstract void handle(TaskContext context);
 
     protected void next(TaskContext context) {
+        this.lastContext = context;
         if (nextHandler != null) {
             nextHandler.handle(context);
         }
+    }
+
+    public TaskContext getContext() {
+        return lastContext;
     }
 }
