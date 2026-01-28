e4e506122bb75fb559b4335ecfd5dc10cb2ae4ed
Bahar Kimanos <bahar.kimanos@gmail.com>
2026-01-28 19:55:55 +0100

    feat: add fuv and finaldeicisionhandler

--- src/main/java/com/skarbalius/task/FUVHandler.java
+++ src/main/java/com/skarbalius/task/FUVHandler.java
@@ -1,10 +1,31 @@
 package com.skarbalius.task;
 
+import com.skarbalius.input.BooleanOperator;
+import java.util.Vector;
+
 public class FUVHandler extends TaskHandler{
+    private static final int SIZE = 15;
+
     @Override
     public void handle(TaskContext context) {
         context.fuv = new java.util.Vector<>(context.NUMPOINTS);
-        System.out.println("FUVHandler");
+        super.setNextHandler(new FinalDecisionHandler());
+        super.next(context);
+
+        for (int i = 0; i < 15; i++) {
+            if (context.puv.get(i) == false) {
+                context.fuv.add(true);
+            }
+            else {
+                if (context.pum.get(i).contains(false)){
+                    context.fuv.add(false);
+                }
+                else {
+                    context.fuv.add(true);
+                    }
+                }
+            }
+        System.out.println("FUVHandler finished");
         super.setNextHandler(new FinalDecisionHandler());
         super.next(context);
     }
--- src/main/java/com/skarbalius/task/FinalDecisionHandler.java
+++ src/main/java/com/skarbalius/task/FinalDecisionHandler.java
@@ -6,5 +6,15 @@ public class FinalDecisionHandler extends TaskHandler{
         context.decision = false;
         System.out.println("FinalDecisionHandler");
         super.next(context);
+        context.decision = false;
+
+        if (!context.fuv.contains(false)) {
+            context.decision = true;
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+        System.out.println("FinalDecisionHandler finished");
     }
 }
