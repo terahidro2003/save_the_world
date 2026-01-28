46f151f541917b65fb57b5f4a1fe627f867d8d36
Juozas Skarbalius <juozas2003@gmail.com>
2026-01-28 21:19:40 +0100

    refactor: add assertions to integration test (#6)

--- src/test/java/com/skarbalius/IntegrationTest.java
+++ src/test/java/com/skarbalius/IntegrationTest.java
@@ -1,11 +1,18 @@
 package com.skarbalius;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skarbalius.output.Output;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class IntegrationTest {
     private static final File resourcesDirectory = new File("src/test/resources");
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     static {
         if (!resourcesDirectory.exists()) {
@@ -14,10 +21,23 @@ class IntegrationTest {
     }
 
     @Test
-    void testcase1() {
+    @DisplayName("Testcase 1: YES (LICs: 0,2,4,6,9,12 satisfied)")
+    void testcase1() throws IOException {
         DECIDE.execute(
                 new File(resourcesDirectory + "/testcase_1.json"),
                 new File(resourcesDirectory + "/output_testcase_1.json")
         );
+
+        var result = objectMapper.readValue(new File(resourcesDirectory + "/output_testcase_1.json"), Output.class);
+        assertNotNull(result);
+        assertNotNull(result.cmv());
+        assertTrue(result.cmv().get(0));
+        assertTrue(result.cmv().get(2));
+        assertTrue(result.cmv().get(4));
+        assertTrue(result.cmv().get(6));
+        assertTrue(result.cmv().get(9));
+        assertTrue(result.cmv().get(12));
+
+        assertTrue(result.decision());
     }
 }
